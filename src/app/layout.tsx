132a2abefe08da89befcfea3b7f0f2758f4fd530
Artem Tyshchuk <artemtyschukk@gmail.com>
2025-12-06 13:16:57 +0100

    Add CSP nonce retrieval in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // get CSP nonce
   const nonce = getCSPNonce();
   return (
     <html lang="en" nonce={nonce}>
